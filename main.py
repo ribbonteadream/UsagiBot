133d9830d6b97c2f535f125b6eaf1951c5250402
RobbieNeko <30732426+RobbieNeko@users.noreply.github.com>
2026-03-09 20:58:53 -0700

    Add basic moderation base
    The basic moderation functions for UsagiBot, could be expanded in the future.

---
+++ main.py
@@ -0,0 +1,98 @@
+import discord
+from discord.ext import commands
+
+import datetime
+import json
+import os
+
+#Change this to whatever your source code's link is, if you run a modified version
+SOURCE_CODE_URL = "https://github.com/RobbieNeko/UsagiBot"
+MODLOG_CHANNEL_ID = 1222257915973599302
+
+with open("./config.json") as file:
+    config = json.load(file)
+    BOT_TOKEN = config['bot_token']
+
+if not os.path.isfile("./warnlog.json"):
+    with open("./warnlog.json", 'w') as file:
+        # Makes sure that the file is initialized with a blank dictionary if not found
+        # Actual structure is dict[int, list[str]], where the keys are user IDs
+        json.dump({}, file)
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix="u!", intents=intents)
+
+@bot.tree.command()
+async def about(interaction: discord.Interaction):
+    """Prints basic 'about' info"""
+    info = discord.Embed(title='About UsagiBot')
+    info.add_field(name="Developer(s)", value="RosaAeterna (aka NekoRobbie), RibbonTeaDream")
+    info.add_field(name="Library", value="Discord.py")
+    info.add_field(name="License", value="GNU AGPL v3")
+    await interaction.response.send_message(embed=info)
+
+@bot.tree.command()
+@discord.app_commands.default_permissions(ban_members=True)
+@discord.app_commands.checks.has_permissions(ban_members=True)
+async def ban(interaction: discord.Interaction, user: discord.Member, reason: str | None = None):
+    """Bans a user from the guild, sending a DM to them as well as putting it in the mod log"""
+    channel = bot.get_channel(MODLOG_CHANNEL_ID)
+    if type(channel) == discord.TextChannel:
+        await channel.send(f"{user.display_name} has been banned by {interaction.user.display_name}!{f"\n{reason}" if reason != None else ''}")
+    await user.send(f"You have been banned from {interaction.guild}!{f"\n{reason}" if reason != None else ''}")
+    await user.ban(reason=reason)
+    await interaction.response.send_message(f"Banned {user.display_name}!", ephemeral=True)
+
+@bot.tree.command()
+@discord.app_commands.default_permissions(kick_members=True)
+@discord.app_commands.checks.has_permissions(kick_members=True)
+async def kick(interaction: discord.Interaction, user: discord.Member, reason: str | None = None):
+    """Kicks a user from the guild, sending a DM to them as well as putting it in the mod log"""
+    channel = bot.get_channel(MODLOG_CHANNEL_ID)
+    if type(channel) == discord.TextChannel:
+        await channel.send(f"{user.display_name} has been kicked by {interaction.user.display_name}!{f"\n{reason}" if reason != None else ''}")
+    await user.send(f"You have been kicked from {interaction.guild}!{f"\n{reason}" if reason != None else ''}")
+    await user.kick(reason=reason)
+    await interaction.response.send_message(f"Kicked {user.display_name}!", ephemeral=True)
+
+@bot.tree.command()
+@discord.app_commands.default_permissions(moderate_members=True)
+@discord.app_commands.checks.has_permissions(moderate_members=True)
+async def timeout(interaction: discord.Interaction, user: discord.Member, length: datetime.timedelta, reason: str | None = None):
+    """Applies a timeout to the user in the guild, sending a DM to them as well as putting it in the mod log"""
+    channel = bot.get_channel(MODLOG_CHANNEL_ID)
+    if type(channel) == discord.TextChannel:
+        await channel.send(f"{user.display_name} has been timed out for {length.total_seconds()} seconds by {interaction.user.display_name}!{f"\n{reason}" if reason != None else ''}")
+    await user.send(f"You have been timed out from {interaction.guild} for {length.total_seconds()} seconds!{f"\n{reason}" if reason != None else ''}")
+    await user.timeout(length, reason=reason)
+    await interaction.response.send_message(f"Timed out {user.display_name} for {length.total_seconds()} seconds!", ephemeral=True)
+
+@bot.tree.command()
+@discord.app_commands.default_permissions(moderate_members=True)
+@discord.app_commands.checks.has_permissions(moderate_members=True)
+async def warn(interaction: discord.Interaction, user: discord.Member, reason: str):
+    """Warns a user in the guild, sending a DM to them as well as putting it in the mod log. Also logs the warning in a file to keep track."""
+    channel = bot.get_channel(MODLOG_CHANNEL_ID)
+    if type(channel) == discord.TextChannel:
+        await channel.send(f"{user.display_name} has been warned by {interaction.user.display_name}!\n{reason}")
+    await user.send(f"You have been warned in {interaction.guild}!\n{reason}")
+    # This is TECHNICALLY blocking, but async file handling doesn't really work
+    # And this is being run on a single core system so threaded approaches probably don't work any better
+    with open("./warnlog.json", 'w') as file:
+        js = json.load(file)
+        warnings: list[str] = js[user.id]
+        warnings.append(reason)
+        js[user.id] = warnings
+        json.dump(js, file)
+    await interaction.response.send_message(f"Warned {user.display_name}!", ephemeral=True)
+
+@bot.command()
+async def sync(ctx):
+    # Only the owner(s) should be able to do this
+    # Necessary for slash commands to populate
+    if bot.is_owner(ctx.author):
+        await bot.tree.sync()
+
+bot.run(BOT_TOKEN)
